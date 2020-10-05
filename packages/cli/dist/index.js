1c9abbac08e6c8e110e70ea0ebbe14642132e0ea
Kevindra Singh <kevindra.singh@gmail.com>
2020-10-05 16:42:58 -0700

    🔨 WIP: Some styling work, figlet, chalk etc.

--- packages/cli/dist/index.js
+++ packages/cli/dist/index.js
@@ -1,44 +1,40 @@
 #!/usr/bin/env node
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const yargs = __importStar(require("yargs"));
 const new_1 = require("./commands/new");
+const chalk = require("chalk");
+const figlet = require("figlet");
 let newCommand = new new_1.NewCommand();
-yargs
-    .scriptName('💬  cjs')
-    .usage('$0|chitchat <cmd> [options]')
-    .command('new', '🚧  Create a new ChitchatJS project.', newCommand.setOptions, newCommand.execute)
-    .command('add', '➕  Create a new talk.', (yargs) => { }, (argv) => {
-    console.log('Command executed.');
-})
-    .command('build', '🔨  Build the project.', (yargs) => { }, (argv) => {
-    console.log('Not implemented yet.');
-})
-    .command('build', '🚀  Deploy the project (requires ask-cli).', (yargs) => { }, (argv) => {
-    console.log('Not implemented yet.');
-})
-    .command('build', '⛔  Cleanup.', (yargs) => { }, (argv) => {
-    console.log('Not implemented yet.');
-})
-    .help()
-    .argv;
+const yargs = require("yargs");
+console.log(chalk.yellowBright(figlet.textSync("Chit chat JS")));
+function setupCommands() {
+    let args = yargs
+        .scriptName(chalk.green('cjs'))
+        .usage(`$0 ${chalk.blue("[cmd]")} ${chalk.magenta("[options]")}`)
+        .command('new', '🚧 Create a new ChitchatJS project.', newCommand.setOptions, newCommand.execute)
+        .command('add', '➕ Create a new talk.', (yargs) => { }, (argv) => {
+        console.log('Command executed.');
+    })
+        .command('build', '🔨 Build the project.', (yargs) => { }, (argv) => {
+        console.log('Not implemented yet.');
+    })
+        .command('deploy', '🚀 Deploy the project (requires ask-cli).', (yargs) => { }, (argv) => {
+        console.log('Not implemented yet.');
+    })
+        .command('clean', '⛔ Clean up.', (yargs) => { }, (argv) => {
+        console.log('Not implemented yet.');
+    })
+        .alias('h', 'help')
+        .help('help')
+        .usage('Usage: $0 -x [num]')
+        .describe('help', "See what's available!")
+        .showHelpOnFail(true, "Specify --help for available options")
+        .alias('v', 'version')
+        .describe('version', 'Show current version of CJS.')
+        .strict()
+        .argv;
+    yargs.showHelp();
+    process.exit(0);
+}
+setupCommands();
 //# sourceMappingURL=index.js.map
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,26 +1,53 @@
 #!/usr/bin/env node
 // import yargs = require('yargs')
 
-import * as yargs from 'yargs'
+// import * as yargs from 'yargs'
 import { NewCommand } from './commands/new'
+// import * as chalk from 'chalk'
+import chalk = require('chalk')
+import figlet = require('figlet')
 
 let newCommand = new NewCommand()
 
-yargs
-    .scriptName('💬  cjs')
-    .usage('$0|chitchat <cmd> [options]')
-    .command('new', '🚧  Create a new ChitchatJS project.', newCommand.setOptions, newCommand.execute)
-    .command('add', '➕  Create a new talk.', (yargs) => {}, (argv) => {
-        console.log('Command executed.')
-    })
-    .command('build', '🔨  Build the project.', (yargs) => {}, (argv) => {
-        console.log('Not implemented yet.')
-    })
-    .command('build', '🚀  Deploy the project (requires ask-cli).', (yargs) => {}, (argv) => {
-        console.log('Not implemented yet.')
-    })
-    .command('build', '⛔  Cleanup.', (yargs) => {}, (argv) => {
-        console.log('Not implemented yet.')
-    })
-    .help()
-    .argv
+import yargs = require('yargs');
+console.log(chalk.yellowBright(figlet.textSync("Chit chat JS")))
+
+// Yargs cheatsheet
+// https://devhints.io/yargs
+
+function setupCommands() {
+    let args = yargs
+        .scriptName(chalk.green('cjs'))
+        .usage(`$0 ${chalk.blue("[cmd]")} ${chalk.magenta("[options]")}`)
+        // .command("", "Hello Command", (yargs) => { console.log('welcome!!')}, (argv) => { console.log('welcome' + argv) })
+        .command('new', '🚧 Create a new ChitchatJS project.', newCommand.setOptions, newCommand.execute)
+        .command('add', '➕ Create a new talk.', (yargs) => { }, (argv) => {
+            console.log('Command executed.')
+        })
+        .command('build', '🔨 Build the project.', (yargs) => { }, (argv) => {
+            console.log('Not implemented yet.')
+        })
+        .command('deploy', '🚀 Deploy the project (requires ask-cli).', (yargs) => { }, (argv) => {
+            console.log('Not implemented yet.')
+        })
+        .command('clean', '⛔ Clean up.', (yargs) => { }, (argv) => {
+            console.log('Not implemented yet.')
+        })
+
+        .alias('h', 'help')
+        .help('help')
+        .usage('Usage: $0 -x [num]')
+        .describe('help', "See what's available!")
+        .showHelpOnFail(true, "Specify --help for available options")
+
+        .alias('v', 'version')
+        .describe('version', 'Show current version of CJS.')
+
+        .strict()
+        .argv
+
+    yargs.showHelp()
+    process.exit(0)
+}
+
+setupCommands()
