a5945f5c8c550fd6e3a964a9f75a34ed03f16424
Kevindra Singh <kevindra.singh@gmail.com>
2020-10-06 19:05:14 -0700

    🔨 WIP: removing dist from the src control

--- packages/alexa/dist/dialog-management/default/InteractionExecutor.js
+++ packages/alexa/dist/dialog-management/default/InteractionExecutor.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.DefaultInteractionExecutor = void 0;
 class DefaultInteractionExecutor {
     execute(handlerInput, interaction) {
         var action = interaction.system.actions[0];
