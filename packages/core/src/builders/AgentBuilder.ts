3d895deac6eb23bfd3797b630709e06f3d30a9d4
Kevindra Singh <kevindra.singh@gmail.com>
2020-10-12 22:10:03 -0700

    :white_check_mark: (Core) Adding test setup with 2 tests.
    #3

--- packages/core/src/builders/AgentBuilder.ts
+++ packages/core/src/builders/AgentBuilder.ts
@@ -1,7 +1,12 @@
 import { BuilderContext, Agent, DialogContext, State, Event } from "../models";
+import * as yup from "yup";
+import * as lodash from "lodash";
 
 export class AgentBuilder<B extends BuilderContext, D extends DialogContext, E extends Event> {
     private _states: { [name: string]: State<B, D, E> };
+    private _agentSchema = yup.object().shape({
+        states: yup.object().required(),
+    });
 
     constructor() {
         this._states = {};
@@ -12,10 +17,18 @@ export class AgentBuilder<B extends BuilderContext, D extends DialogContext, E e
         return this;
     }
 
-    build(): Agent<B, D, E> {
-        return {
+    async build(): Promise<Agent<B, D, E>> {
+        return await this._validate({
             type: "Agent",
             states: this._states,
-        };
+        });
+    }
+
+    private async _validate(agent: Agent<B, D, E>) {
+        await this._agentSchema.validate(agent);
+        if (Object.keys(agent.states).length === 0) {
+            throw new Error("states is a required field");
+        }
+        return agent;
     }
 }
---
+++ packages/core/test/builders/AgentBuilderTest.ts
@@ -0,0 +1,34 @@
+import { AgentBuilder } from "../../src/builders/AgentBuilder";
+import { BuilderContext, DialogContext, Event, State } from "../../src/models";
+import { expect } from "chai";
+import "mocha";
+
+const mockState: State<BuilderContext, DialogContext, Event> = {
+    type: "State",
+    name: "mock-state",
+    block: {
+        build: () => {},
+        execute: () => {},
+    },
+};
+
+describe("AgentBuilder", () => {
+    it("should throw validation error if missing states", async () => {
+        let err: Error = new Error();
+        try {
+            let ab = new AgentBuilder();
+            await ab.build();
+        } catch (e) {
+            err = e;
+        }
+        expect(err.message).to.be.equal("states is a required field");
+    });
+
+    it("should successfully build agent with one state", async () => {
+        let agent = await new AgentBuilder().addState(mockState).build();
+
+        expect(agent, "agent is null").to.not.be.null;
+        expect(agent.states, "states is null").to.not.be.null;
+        expect(Object.keys(agent.states).length, "states length is not 1").to.be.equal(1);
+    });
+});
