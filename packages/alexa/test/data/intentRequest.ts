238a96605bcbd008f0dda39dfec0de5e40de7c70
Kevindra Singh <kevindra.singh@gmail.com>
2020-10-14 23:46:25 -0700

    chore: added some test data requests and logger util

---
+++ packages/alexa/test/data/intentRequest.ts
@@ -0,0 +1,48 @@
+import { RequestEnvelope } from "ask-sdk-model";
+
+export let intentRequest: RequestEnvelope = {
+  version: "1.0",
+  session: {
+    new: false,
+    sessionId: "amzn1.echo-api.session.123456789012",
+    application: {
+      applicationId: "amzn1.ask.skill.987654321",
+    },
+    attributes: {},
+    user: {
+      userId: "amzn1.ask.account.testUser",
+    },
+  },
+  context: {
+    AudioPlayer: {
+      playerActivity: "IDLE",
+    },
+    System: {
+      apiEndpoint: "dummy-api-end-point",
+      application: {
+        applicationId: "amzn1.ask.skill.987654321",
+      },
+      user: {
+        userId: "amzn1.ask.account.testUser",
+      },
+      device: {
+        deviceId: "random-device-id",
+        supportedInterfaces: {
+          AudioPlayer: {},
+        },
+      },
+    },
+  },
+  request: {
+    type: "IntentRequest",
+    requestId: "amzn1.echo-api.request.1234",
+    timestamp: "2016-10-27T21:06:28Z",
+    locale: "en-US",
+    intent: {
+      name: "HelloIntent",
+      slots: {},
+      confirmationStatus: "NONE",
+    },
+    dialogState: "IN_PROGRESS",
+  },
+};
