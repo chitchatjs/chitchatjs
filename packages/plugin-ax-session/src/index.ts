7befbcfeb22a9121c51f5eba0f157e597f8008fa
Kevindra Singh <kevindra.singh@gmail.com>
2020-10-29 16:04:21 -0700

    fix: adding real code now
    re #98

--- packages/plugin-ax-session/src/index.ts
+++ packages/plugin-ax-session/src/index.ts
@@ -1,4 +1,6 @@
 // import _end from "./blocks/end";
+import { AlexaBuilderContext, AlexaDialogContext, AlexaEvent, ax, Locale } from "@chitchatjs/alexa";
+import { CompoundBlock } from "../../alexa/node_modules/@chitchatjs/core/dist";
 
 /**
  * Session management building blocks for Alexa Skills.
@@ -9,5 +11,20 @@ export namespace session {
    *
    * @param shouldEnd true|false|undefined
    */
-  // export let end = _end;
+  export let end = (
+    shouldEnd?: boolean
+  ): CompoundBlock<AlexaBuilderContext, AlexaDialogContext, AlexaEvent> => {
+    return ax
+      .compound()
+      .add(
+        ax
+          .custom()
+          .executor((c: AlexaDialogContext, e: AlexaEvent) => {
+            c.currentResponse.response.shouldEndSession = shouldEnd;
+            return c.currentResponse;
+          })
+          .build()
+      )
+      .build();
+  };
 }
